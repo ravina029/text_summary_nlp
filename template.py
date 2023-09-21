d3ee00b1e3cf36a0352d55828388df84c1804a00
ravina <vermaravina029@gmail.com>
2023-09-21 16:39:26 +0300

    folder structure added

---
+++ template.py
@@ -0,0 +1,47 @@
+import os
+from pathlib import Path 
+import logging #because we want to log all the info in real time
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]:%(message)s:')
+
+
+project_name='textSummarizer'
+
+list_of_files=[
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common_.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "trails.ipynb"
+    
+]
+
+for filepath in list_of_files:
+    filepath=Path(filepath)
+    filedir, filename=os.path.split(filepath) #to separate the folder and file 
+    
+
+    if filedir !="":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Creating directory:{filedir} for the ffile {filename}")
+
+    if (not os.path.exists(filepath) or (os.path.getsize(filepath))==0):
+        with open(filepath,"w") as f:
+            pass
+            logging.info(f'Creating empty file: {filepath}')
+
+    else:
+        logging.info(f"{filename} already exists")
